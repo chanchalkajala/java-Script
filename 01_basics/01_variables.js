148cf4c7f8d8d3660c171d5e5beef87e0a4cfc46
pallu chanchal <chanchalkajala037@gmail.com>
2026-04-01 14:33:06 +0000

    maine changed kiya hai accountState

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 12345
+let accountEmail = "pallu123@gmail.com"
+var accountPassword = "1234"
+accountCity = "patna"
+let accountState;
+
+
+// accountId = 2 not allowed
+
+accountEmail = "apna234@gmail.com"
+
+accountPassword = "232323"
+accountCity = " delhi"
+
+// console.log(accountId);
+
+/*
+prefer not to us var
+because of issue in block scope and funcational scope
+/*
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
