6242b592b2120e81bcbc860a0a0d7a2ddf081110
pallu chanchal <chanchalkajala037@gmail.com>
2026-04-03 11:37:38 +0000

    strict add

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// ye savi console output ayega boolein type me 
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// console.log(null > 0);   ouput false  conversion
+// console.log(null == 0); ouput false   conversion
+// console.log(null >= 0); ouput true    conversion
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+// === Strict Equality Operator
+// 🔍 Kaam kya karta hai? Ye value + data type dono check karta hai
+
+console.log("2" === 2);  // output false
