8ec36b26ef55bd929536faecbf6553423b98a0cc
pallu chanchal <chanchalkajala037@gmail.com>
2026-04-03 01:14:35 +0000

    conversion of deta types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "pallu"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number (score)
+console.log(valueInNumber);
+
+// "3" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = ""
+
+let bollenIsLoggedIn = Boolean(isLoggedIn);
+console.log( bollenIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "pallu" => true
+
