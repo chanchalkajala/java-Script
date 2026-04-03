0a62da388e7d373e6e9a7d6af5c95779af757155
pallu chanchal <chanchalkajala037@gmail.com>
2026-04-03 02:41:02 +0000

    next code add

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,21 +1,59 @@
-let score = "pallu"
+        // *************** converation******************
+  
+  
+ // let score = "pallu"
 
-console.log(typeof score);
-console.log(typeof(score));
+ //console.log(typeof score);
+  //console.log(typeof(score));
 
-let valueInNumber = Number (score)
-console.log(valueInNumber);
+// let valueInNumber = Number (score)
+// console.log(valueInNumber);
 
 // "3" => 33
 // "33abc" => NaN
 // true => 1; false => 0
 
-let isLoggedIn = ""
+//let isLoggedIn = ""
 
-let bollenIsLoggedIn = Boolean(isLoggedIn);
-console.log( bollenIsLoggedIn);
+// let bollenIsLoggedIn = Boolean(isLoggedIn);
+// console.log( bollenIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
 // "pallu" => true
 
+//******************* operation*****************************
+
+ let value = 3
+ let negValue = -value
+   //console.log(negValue);
+
+   //******** string ko v jora ja skta hai jaise *******
+
+   let str1 = "pallu";
+   let str2 = "chanchal";
+   let str3 = str1 + str2;
+  // console.log(str3);
+
+  // console.log("1" + 2);  //output => 12
+  // console.log(1 + 2);    //output => 3
+  // console.log(1 + "2");  //output => 12
+  // console.log("1" + 2 + 2); //output => 122
+  // console.log(1 + 2 + "4"); //output => 34
+  // console.log(1 + "2" + "4"); //output => ?
+  // console.log("1" + "2" + "4"); //output => ?
+  // console.log("1" + 2 + "4"); //output => ?
+
+
+  // console.log(+true);   // output => 1
+  // console.log(true+);   // output => it is not allow
+
+  // console.log(+"");  // output => 0
+
+  let gameCounter = 100;
+//   ++gameCounter;      esko bolte hai hm Prefix Increment bolte hain  Pehle value increase hoti hai
+// Phir value use hoti hai
+
+    gameCounter++;    // esko bolte hai  Postfix Increment bolte hain  Pehle value use hoti hai Phir value increase hoti hai
+
+  console.log(gameCounter);
