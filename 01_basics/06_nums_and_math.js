c869a860c82dd1777a8e843be8e93593527abc2b
pallu chanchal <chanchalkajala037@gmail.com>
2026-04-09 06:00:26 +0000

    let cahnge to random no

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,7 +1,36 @@
+// *************** Numbers ****************
+
 const score  = 500;
-console.log(score);
+
+// console.log(score);
 
 const balance = new Number(200);
-console.log(balance);
+// console.log(balance);
 
-console.log(balance.toString(). length);
+// console.log(balance.toString(). length);
+
+// console.log(balance.toFixed(1));
+
+const otherNumber = 123.6789;
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString()); // output is us velue 
+// console.log(hundreds.toLocaleString("en-in")); // output is in velue
+
+// ******************** Maths **********************
+// console.log(Math);
+
+// console.log(Math.abs(-9));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+
+
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+
+console.log(Math.random());
+
+
