c237a44576760dcddba3a361cab8974625b7c1dd
pallu chanchal <chanchalkajala037@gmail.com>
2026-05-24 06:25:12 +0000

    next code add

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,3 +1,27 @@
 // array 
 
 const myArr = [0, 1, 2, 3, 4, 5]
+const myHero = ["palluchanchal", "chanchalkajala"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr[1]);
+
+// Array methods
+
+// **********
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+console.log(myArr);
+
+// **********
+
+
+// **********
+
+// myArr.unshift(9)
+// myArr.shift()
+
+console.log(myArr.includes(7))
+
+console.log(myArr);
