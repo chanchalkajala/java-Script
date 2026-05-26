d9544e44fd2c50c2cf216d696182501ef18a8180
pallu chanchal <chanchalkajala037@gmail.com>
2026-05-26 19:18:04 +0000

    next code added

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -5,12 +5,38 @@
 // singleton
 // object.create
 
+// ****************
 
 // object literals
+// ***************
+
+const mySym = Symbol("key1")
+
+
+
+// ***************
+
+
+
+// ***************
 const jsUser = {
     name: "pallu",
+    "full name": "pallu kumar",
+    [mySym]: "mykey1",
     age:  19,
+    location: "patna",
+    email:  "pallu89@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["monday", "sunday"]
     
 }
 
+// esko acces kaise kre 
+
+console.log(jsUser.email);
+console.log(jsUser["email"]);
+console.log(jsUser["full name"]);
+console.log(jsUser[mySym]);
+
+// *****************
 
