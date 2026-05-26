9b405afde9948d2fc380f1e797b050329fafaa7e
pallu chanchal <chanchalkajala037@gmail.com>
2026-05-26 18:33:25 +0000

    start code

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,16 @@
+// object deceler krne ke tarike chatgpt se smjh lo ok importent
+// ye auper ka line apne man se hai ok class ka part nhi hai diyan dena
+
+
+// singleton
+// object.create
+
+
+// object literals
+const jsUser = {
+    name: "pallu",
+    age:  19,
+    
+}
+
+
