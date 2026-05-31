c401203f5ce26df4fd792538ceed650449c3af41
pallu chanchal <chanchalkajala037@gmail.com>
2026-05-31 17:12:58 +0000

    code added

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -3,4 +3,33 @@
 // const tinderUser = new Object();  // singleton object
 const tinderUser = {}  // non singleton object
 
-console.log(tinderUser);
+
+tinderUser.id = "1234abc",
+tinderUser.name = "pallu",
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+// *************
+
+const regularUser = {
+    email: "chanchal@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname: "pallu",
+            lastname: "kumar"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname); 
+
+// **********
+
+const obj1 = {1: "a", 2: "b"};
+const obj2 = {3: "a", 4: "b"};
+const obj2 = {3: "a", 4: "b"};
+
+// const obj3 = {obj1, obj2}
+const obj3 = Object.assign({}, obj1, obj2)
+console.log(obj3);
