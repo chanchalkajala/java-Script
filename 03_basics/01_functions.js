1052868ff7ad28db7cc21a9518c5735242969d62
pallu chanchal <chanchalkajala037@gmail.com>
2026-06-01 05:55:40 +0000

    added new code

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,30 @@
+function sayMyName (){
+    console.log("p")
+    console.log("a")
+    console.log("l")
+    console.log("l")
+    console.log("u")
+}
+
+// sayMyName()
+
+// *********
+                       // pramiter
+// function addTwoNumber(number1, number2){
+//     console.log(number1 + number2);
+// }
+
+//  const result = addTwoNumber(3, 4);  //try it 
+
+function addTwoNumber(number1, number2){
+    // let result = number1 + number2
+    // return result
+
+    return number1 + number2
+}
+
+ const result = addTwoNumber(3, 4);  //try it 
+ console.log("Result:", result);
+// addTwoNumber(3, "4"); // try it 
+             // argoments
+// addTwoNumber(3, null); 
