33e3a9228992de9b0bea9eae40f5b51e6da3d293
pallu chanchal <chanchalkajala037@gmail.com>
2026-06-09 13:08:06 +0000

    next code added

---
+++ 04_control_flow/one.js
@@ -0,0 +1,29 @@
+// if
+const isUserloggedIn = true
+const temperature = 41
+// if (temperature === 40) {
+//     console.log("less then 50")
+
+
+// } else {
+//     console.log("temperature is greater than 50");
+
+// }
+
+// <, >, <=, >=, ==, !=, ===, !== 
+
+// note scope se releted code
+
+// const score = 200
+
+// if (score > 100){
+//     let power = "fly"
+//     console.log(`user power: ${power}`)
+
+// }
+
+//     console.log(`user power: ${power}`)
+
+const balance = 1000
+
+if (balance > 500) console.log("test"), console.log("test2");
