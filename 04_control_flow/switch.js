2a2a01fb21eb08de98c20ff4ff244342d7b57c68
pallu chanchal <chanchalkajala037@gmail.com>
2026-06-10 02:24:00 +0000

    add switch code added

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,30 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march" // esme stirg match and number match hota hai
+
+switch (month) {
+    case "january":
+        console.log("january");
+        break;
+
+    case "febury":
+        console.log("febury");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match")
+        break;
+}
