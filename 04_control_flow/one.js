9ec52fb441153a6c71bedb24d35ba9ebb6ac5057
pallu chanchal <chanchalkajala037@gmail.com>
2026-06-10 01:29:56 +0000

    the last code condition check

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -24,6 +24,33 @@ const temperature = 41
 
 //     console.log(`user power: ${power}`)
 
-const balance = 1000
+// const balance = 1000
 
-if (balance > 500) console.log("test"), console.log("test2");
+// if (balance > 500) console.log("test"), console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less then 500");
+
+// } else if (balance < 750) {
+//     console.log("less than 750");
+
+// } else if (balance < 900) {
+//     console.log("less than 900")
+
+// } else {
+//     console.log("less than 1200")
+// }
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard){
+    console.log("Allow to buy course");
+
+}if(loggedInFromGoogle ||  loggedInFromEmail ){
+  console.log("user logged in")
+}
+
