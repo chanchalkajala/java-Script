6a3ab6bd4ec5d61802b10d7dd4ffbb6c16561870
pallu chanchal <chanchalkajala037@gmail.com>
2026-06-13 01:33:17 +0000

    the last code added map pr loop for in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,37 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+    swift:'swift by apple'
+}
+// object pr loop
+for (const key in myObject) {
+   
+    // console.log( myObject [key]);
+    // console.log( `${key} shortcut is four ${myObject[key]}`);
+    
+}
+
+// four in kiya array pr kam karegi  note array ki key docomentesion padho jake
+
+const programing = ["js", "rb", "java", "cpp"]
+
+for (const key in programing) {
+//    console.log( programing [key])
+    
+}
+
+// map pr for in loop ess trh hm nhi kr skte hai
+const map = new Map()
+map.set(`IN, "India`)
+map.set(`USA, "United states of America`)
+map.set(`Fr, "France`)
+map.set(`Fr, "France`)
+
+
+// for (const key in map) {
+//     console.log( map [key])
+    
+    
+// }
+
