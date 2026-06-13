a5764a8c6df0b2f4819d40f233559b7378a09498
pallu chanchal <chanchalkajala037@gmail.com>
2026-06-13 02:31:59 +0000

    to code added forEach  in arr

---
+++ 05_iterations/five.js
@@ -0,0 +1,41 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+coding.forEach( function (val) {
+    // console.log(val);
+})
+
+// coding.forEach ((val) => {
+//     console.log(val);
+
+// })
+
+// function printMe(item) {
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+
+
+// coding.forEach((item,index,arr) => {
+//     console.log(item, index, arr);
+// })
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "nodejs",
+        languageFileName: "node"
+    }
+];
+
+myCoding.forEach((item) => {
+    console.log(item.languageFileName);
+});
