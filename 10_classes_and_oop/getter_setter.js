061a76cada1d1ac3b54e80a389eab9ead2719ef8
pallu chanchal <chanchalkajala037@gmail.com>
2026-07-17 12:01:13 +0000

    the last code add get set email

---
+++ 10_classes_and_oop/getter_setter.js
@@ -0,0 +1,31 @@
+class User {
+    constructor(email, password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getter
+    get email() {
+        return this._email.toUpperCase();
+    }
+
+    // Setter
+    set email(value) {
+        this._email = value;
+    }
+
+    // Getter
+    get password() {
+        return `${this._password}maya`;
+    }
+
+    // Setter
+    set password(value) {
+        this._password = value;
+    }
+}
+
+const pallu = new User("maya@.ai", "abc");
+
+console.log(pallu.email);      // MAYA@.AI
+console.log(pallu.password);   // abcmaya
