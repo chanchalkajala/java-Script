f53d7172c8443c603dbbbba8f628d1a28b5e4911
pallu chanchal <chanchalkajala037@gmail.com>
2026-07-17 11:25:47 +0000

    new file added

--- 10_classes_and_oop/mathpi.js
+++ 10_classes_and_oop/mathpi.js
@@ -9,7 +9,25 @@ const descripter = Object.getOwnPropertyDescriptors(Math, "PI");
 const chai = {
     name: 'ginger chai',
     price: 250,
-    isAvailable: true
-}
-console.log(chai);
+    isAvailable: true, 
 
+    orderChai: function(){
+        console.log("chai nhi bani ")
+    }
+}
+// console.log(Object.getOwnPropertyDescriptors(chai, "name"));
+
+Object.defineProperty(chai, 'name',{
+    // writable: false,
+    enumerable: false
+})
+
+console.log(Object.getOwnPropertyDescriptors(chai, "name"));
+
+
+for (let [key, value] of Object.entries(chai)) {
+    if (typeof value !== 'function') {
+         console.log(`${key} : ${value}`)
+    }
+    
+}
