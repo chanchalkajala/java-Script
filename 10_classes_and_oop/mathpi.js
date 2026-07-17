c6244f4f5ec007d0c7dd918f0fa49bd0229713ac
pallu chanchal <chanchalkajala037@gmail.com>
2026-07-17 05:58:38 +0000

    new code added

---
+++ 10_classes_and_oop/mathpi.js
@@ -0,0 +1,15 @@
+const descripter = Object.getOwnPropertyDescriptors(Math, "PI");
+// console.log(descripter);
+
+
+// console.log(Math.PI);
+
+// const mynewObject = Object.create(null);
+
+const chai = {
+    name: 'ginger chai',
+    price: 250,
+    isAvailable: true
+}
+console.log(chai);
+
